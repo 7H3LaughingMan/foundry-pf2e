87009f8b03cf89b4bd95b33f09f338f5cc6b662d
7H3LaughingMan <7H3LaughingMan@proton.me>
2024-10-15 10:44:46 -0500

    PF2e Modifiers Matter

---
+++ types/pf2e-modifiers-matter/index.d.ts
@@ -0,0 +1,136 @@
+import { ActorPF2e } from "../pf2e/module/actor/base.js";
+import { RawModifier } from "../pf2e/module/actor/modifiers.js";
+import { ChatMessagePF2e } from "../pf2e/module/chat-message/document.js";
+import { ItemPF2e } from "../pf2e/module/item/base/document.js";
+import { TokenDocumentPF2e } from "../pf2e/module/scene/token-document/document.js";
+
+export { };
+
+export namespace pf2eModifiersMatter {
+    export type Degree = "CRIT_SUCC" | "SUCCESS" | "FAILURE" | "CRIT_FAIL";
+
+    export type SignificantModifier = {
+        appliedTo: "roll" | "dc";
+        name: string;
+        value: number;
+        significance: "ESSENTIAL" | "HELPFUL" | "HARMFUL" | "DETRIMENTAL";
+    };
+
+    export type InsignificantModifier = {
+        appliedTo: "roll" | "dc";
+        name: string;
+        value: number;
+        significance: "NONE";
+    };
+}
+
+declare global {
+    interface Window {
+        pf2eMm?: {
+            getSignificantModifiersOfMessage(chatMessage: ChatMessagePF2e): pf2eModifiersMatter.SignificantModifier[];
+
+            checkIfChatMessageShouldHaveHighlights(chatMessage: ChatMessagePF2e): boolean;
+
+            exampleHookCourageousAnthem(): Function;
+
+            DEGREES: {
+                ESSENTIAL: "ESSENTIAL";
+                HELPFUL: "HELPFUL";
+                NONE: "NONE";
+                HARMFUL: "HARMFUL";
+                DETRIMENTAL: "DETRIMENTAL";
+            };
+
+            IGNORED_MODIFIER_LABELS: Set<string>;
+
+            IGNORED_MODIFIER_SLUGS: Set<string>;
+
+            IGNORED_MODIFIER_LABELS_FOR_AC_ONLY: Set<string>;
+
+            parsePf2eChatMessageWithRoll(chatMessage: ChatMessagePF2e): {
+                rollingActor: ActorPF2e;
+                deltaFromDc: number;
+                dieRoll: number;
+                currentDegreeOfSuccess: pf2eModifiersMatter.Degree;
+                dcSlug: string;
+                isStrike: boolean;
+                isSpell: boolean;
+                targetedToken?: TokenDocumentPF2e;
+                targetedActor?: ActorPF2e;
+                originItem?: ItemPF2e;
+                allModifiersInChatMessage: RawModifier[];
+            };
+
+            filterRollModsFromChatMessage({
+                allModifiersInChatMessage,
+                rollingActor,
+                isStrike
+            }: {
+                allModifiersInChatMessage: RawModifier[];
+                rollingActor: ActorPF2e;
+                isStrike: boolean;
+            }): RawModifier[];
+
+            getDcModsAndDcActor({
+                targetedActor,
+                originItem,
+                dcSlug,
+                isStrike,
+                isSpell,
+                contextOptionsInFlags,
+                chatMessageFlavor
+            }: {
+                targetedActor?: TokenDocumentPF2e;
+                originItem?: ItemPF2e;
+                dcSlug: string;
+                isStrike: boolean;
+                isSpell: boolean;
+                contextOptionsInFlags: string[];
+                chatMessageFlavor: string;
+            }): {
+                dcMods: RawModifier[];
+                actorWithDc?: ActorPF2e;
+            }
+
+            calcSignificantModifiers({
+                rollMods,
+                dcMods,
+                originalDeltaFromDc,
+                dieRoll,
+                currentDegreeOfSuccess,
+                isStrike
+            }: {
+                rollMods: RawModifier[];
+                dcMods: RawModifier[];
+                originalDeltaFromDc: number;
+                dieRoll: number;
+                currentDegreeOfSuccess: pf2eModifiersMatter.Degree;
+                isStrike: boolean;
+            }): {
+                significantRollModifiers: pf2eModifiersMatter.SignificantModifier[];
+                significantDcModifiers: pf2eModifiersMatter.SignificantModifier[];
+                insignificantDcModifiers: pf2eModifiersMatter.InsignificantModifier[];
+            }
+
+            checkHighlightPotentials({
+                rollMods,
+                dcMods,
+                originalDeltaFromDc,
+                dieRoll,
+                currentDegreeOfSuccess,
+                isStrike
+            }: {
+                rollMods: RawModifier[],
+                dcMods: RawModifier[],
+                originalDeltaFromDc: number,
+                dieRoll: number,
+                currentDegreeOfSuccess: pf2eModifiersMatter.Degree,
+                isStrike: boolean
+            }): {
+                plus1StatusHasPotential: boolean;
+                plus2StatusHasPotential: boolean;
+                plus2CircumstanceAcHasPotential: boolean;
+            }
+        }
+    }
+}
---
+++ types/pf2e-toolbelt/targetHelper.d.ts
@@ -0,0 +1,40 @@
+import { SaveType } from "../pf2e/module/actor/types.js";
+import { RollNoteSource } from "../pf2e/module/notes.js";
+import { DegreeAdjustmentsRecord, DegreeOfSuccessString } from "../pf2e/module/system/degree-of-success.js";
+
+export {};
+
+type MessageSaveFlag = {
+    statistic: SaveType;
+    basic: boolean;
+    dc: number;
+    author?: ActorUUID;
+};
+
+type MessageTargetSave = {
+    private: boolean;
+    value: number;
+    die: number;
+    success: DegreeOfSuccessString;
+    roll: string;
+    notes: RollNoteSource[];
+    dosAdjustments: DegreeAdjustmentsRecord | undefined;
+    unadjustedOutcome?: DegreeOfSuccessString | null;
+    modifiers: { label: string; modifier: number }[];
+};
+
+type MessageFlagType = "damage" | "spell-damage" | "spell-save" | "action" | "check";
+
+type MessageFlag = {
+    type?: MessageFlagType;
+    targets?: TokenDocumentUUID[];
+    save?: MessageSaveFlag;
+};
+
+declare module "../foundry/common/documents/chat-message.js" {
+    interface ChatMessageFlags {
+        "pf2e-toolbelt"?: {
+            targetHelper?: MessageFlag;
+        };
+    }
+}
