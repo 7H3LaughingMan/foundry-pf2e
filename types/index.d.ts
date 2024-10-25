2d5f6607bae22986aa195ae6c09a7a9e7b09ec6c
7H3LaughingMan <7H3LaughingMan@proton.me>
2024-10-25 10:00:37 -0500

    Update Exports

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,17 @@
 import "./foundry/index.d.ts";
 import "./pf2e/global.d.ts";
 
+export type * from "./pf2e/module/actor/data/index.d.ts";
 export type * from "./pf2e/module/actor/index.d.ts";
+export type * from "./pf2e/module/actor/modifiers.d.ts";
+export type * from "./pf2e/module/actor/types.d.ts";
 export type * from "./pf2e/module/chat-message/index.d.ts";
 export type * from "./pf2e/module/encounter/index.d.ts";
+export type * from "./pf2e/module/item/base/data/index.d.ts";
+export type * from "./pf2e/module/item/condition/index.d.ts";
 export type * from "./pf2e/module/item/index.d.ts";
+export type * from "./pf2e/module/notes.d.ts";
 export type * from "./pf2e/module/scene/index.d.ts";
+export type * from "./pf2e/module/system/rolls.d.ts";
+export type * from "./pf2e/module/system/statistic/index.d.ts";
 export type * from "./pf2e/module/user/index.d.ts";
