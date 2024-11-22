5d7feec14684be07772cfbdf6897bf58a58cbf9d
Idle <651462+reonZ@users.noreply.github.com>
2024-11-22 18:00:35 +0100

    add foundry.utils.AsyncFunction

--- types/foundry/common/utils/module.d.ts
+++ types/foundry/common/utils/module.d.ts
@@ -3,3 +3,11 @@ export * from "./geometry.ts";
 export * from "./helpers.ts";
 // export * from "./logging.mjs";
 export { default as Color } from "./color.ts";
+
+/**
+ * The constructor of an async function.
+ * @type {typeof AsyncFunction}
+ */
+export const AsyncFunction: {
+    new <T>(...args: any[]): (...args: any[]) => Promise<T>;
+};
