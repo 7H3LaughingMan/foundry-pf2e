5ffd8df6dec9246fdf4b7af09e59327ca7e2015c
Idle <651462+reonZ@users.noreply.github.com>
2024-11-22 14:49:36 +0100

    add dialog v2 types

---
+++ types/foundry/client-esm/applications/api/dialog.d.ts
@@ -0,0 +1,187 @@
+import type { ApplicationConfiguration, ApplicationRenderContext, ApplicationRenderOptions } from "../_types.d.ts";
+import type ApplicationV2 from "./application.d.ts";
+
+/**
+ * A lightweight Application that renders a dialog containing a form with arbitrary content, and some buttons.
+ * @extends {ApplicationV2<ApplicationConfiguration & DialogV2Configuration>}
+ *
+ * @example Prompt the user to confirm an action.
+ * ```js
+ * const proceed = await foundry.applications.api.DialogV2.confirm({
+ *   content: "Are you sure?",
+ *   rejectClose: false,
+ *   modal: true
+ * });
+ * if ( proceed ) console.log("Proceed.");
+ * else console.log("Do not proceed.");
+ * ```
+ *
+ * @example Prompt the user for some input.
+ * ```js
+ * let guess;
+ * try {
+ *   guess = await foundry.applications.api.DialogV2.prompt({
+ *     window: { title: "Guess a number between 1 and 10" },
+ *     content: '<input name="guess" type="number" min="1" max="10" step="1" autofocus>',
+ *     ok: {
+ *       label: "Submit Guess",
+ *       callback: (event, button, dialog) => button.form.elements.guess.valueAsNumber
+ *     }
+ *   });
+ * } catch {
+ *   console.log("User did not make a guess.");
+ *   return;
+ * }
+ * const n = Math.ceil(CONFIG.Dice.randomUniform() * 10);
+ * if ( n === guess ) console.log("User guessed correctly.");
+ * else console.log("User guessed incorrectly.");
+ * ```
+ *
+ * @example A custom dialog.
+ * ```js
+ * new foundry.applications.api.DialogV2({
+ *   window: { title: "Choose an option" },
+ *   content: `
+ *     <label><input type="radio" name="choice" value="one" checked> Option 1</label>
+ *     <label><input type="radio" name="choice" value="two"> Option 2</label>
+ *     <label><input type="radio" name="choice" value="three"> Options 3</label>
+ *   `,
+ *   buttons: [{
+ *     action: "choice",
+ *     label: "Make Choice",
+ *     default: true,
+ *     callback: (event, button, dialog) => button.form.elements.choice.value
+ *   }, {
+ *     action: "all",
+ *     label: "Take All"
+ *   }],
+ *   submit: result => {
+ *     if ( result === "all" ) console.log("User picked all options.");
+ *     else console.log(`User picked option: ${result}`);
+ *   }
+ * }).render({ force: true });
+ * ```
+ */
+export default class DialogV2 extends ApplicationV2<DialogV2Configuration> {
+    /**
+     * A utility helper to generate a dialog with yes and no buttons.
+     *
+     * Resolves to true if the yes button was pressed, or false if the no button
+     * was pressed. If additional buttons were provided, the Promise resolves to
+     * the identifier of the one that was pressed, or the value returned by its
+     * callback. If the dialog was dismissed, and rejectClose is false, the
+     * Promise resolves to null.
+     */
+    static confirm(options?: DialogV2ConfirmOptions): Promise<true | false | null>;
+
+    /**
+     * A utility helper to generate a dialog with a single confirmation button.
+     *
+     * Resolves to the identifier of the button used to submit the dialog,
+     * or the value returned by that button's callback. If the dialog was
+     * dismissed, and rejectClose is false, the Promise resolves to null.
+     */
+    static prompt(options?: DialogV2PromptOptions): Promise<any>;
+
+    /**
+     * Spawn a dialog and wait for it to be dismissed or submitted.
+     *
+     * Resolves to the identifier of the button used to submit the
+     * dialog, or the value returned by that button's callback. If the
+     * dialog was dismissed, and rejectClose is false, the Promise
+     * resolves to null.
+     */
+    static wait(options?: DialogV2WaitOptions): Promise<any>;
+
+    protected _renderHTML(context: ApplicationRenderContext, options: ApplicationRenderOptions): Promise<unknown>;
+    protected _replaceHTML(result: unknown, content: HTMLElement, options: ApplicationRenderOptions): void;
+}
+
+/**
+ * @param event The button click event, or a form submission event if the dialog was submitted via keyboard.
+ * @param button If the form was submitted via keyboard, this will be the default button, otherwise the button that was clicked.
+ * @param dialog The dialog element.
+ */
+export type DialogV2ButtonCallback = (
+    event: PointerEvent | SubmitEvent,
+    button: HTMLButtonElement,
+    dialog: HTMLDialogElement
+) => Promise<any>;
+
+export interface DialogV2Button {
+    /** The button action identifier. */
+    action: string;
+    /** The button label. Will be localized. */
+    label: string;
+    /** FontAwesome icon classes. */
+    icon?: string;
+    /** CSS classes to apply to the button. */
+    class?: string;
+    /**
+     * Whether this button represents the default action to take if the user
+     * submits the form without pressing a button, i.e. with an Enter
+     * keypress.
+     */
+    default?: boolean;
+    /**
+     * A function to invoke when the button is clicked. The value returned
+     * from this function will be used as the dialog's submitted value.
+     * Otherwise, the button's identifier is used.
+     */
+    callback?: DialogV2ButtonCallback;
+}
+
+/**
+ * @param result Either the identifier of the button that was clicked to submit the dialog, or the result returned by that button's callback.
+ */
+export type DialogV2SubmitCallback = (result: any) => Promise<void>;
+
+export interface DialogV2Configuration extends ApplicationConfiguration {
+    /**
+     * Modal dialogs prevent interaction with the rest of the UI until they
+     * are dismissed or submitted.
+     */
+    modal?: boolean;
+    /** Button configuration. */
+    buttons: DialogV2Button[];
+    /** The dialog content. */
+    content?: string;
+    /**
+     * A function to invoke when the dialog is submitted. This will not be
+     * called if the dialog is dismissed.
+     */
+    submit?: DialogV2SubmitCallback;
+}
+
+export interface DialogV2WaitOptions extends ApplicationConfiguration, DialogV2Configuration {
+    /** A synchronous function to invoke whenever the dialog is rendered. */
+    render?: DialogV2RenderCallback;
+    /** A synchronous function to invoke when the dialog is closed under any circumstances. */
+    close?: DialogV2CloseCallback;
+    /** Throw a Promise rejection if the dialog is dismissed. (default: true) */
+    rejectClose?: boolean;
+}
+
+export interface DialogV2PromptOptions extends DialogV2WaitOptions {
+    /** Options to overwrite the default confirmation button configuration. */
+    ok?: Partial<DialogV2Button>;
+}
+
+export interface DialogV2ConfirmOptions extends DialogV2WaitOptions {
+    /** Options to overwrite the default yes button configuration. */
+    yes?: Partial<DialogV2Button>;
+    /** Options to overwrite the default no button configuration. */
+    no?: Partial<DialogV2Button>;
+}
+
+/**
+ * @param event The render event.
+ * @param dialog The dialog element.
+ */
+export type DialogV2RenderCallback = (event: Event, dialog: HTMLDialogElement) => void;
+
+/**
+ * @param event The close event.
+ * @param  dialog The dialog instance.
+ */
+export type DialogV2CloseCallback = (event: Event, dialog: DialogV2) => void;
