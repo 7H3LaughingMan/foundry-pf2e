8d0986cb08057b9eb89196b4356515ebc8b816c9
Idle <651462+reonZ@users.noreply.github.com>
2024-11-22 23:14:49 +0100

    Add `EventEmitterMixin` (#4)

--- types/foundry/client-esm/applications/api/application.d.ts
+++ types/foundry/client-esm/applications/api/application.d.ts
@@ -6,12 +6,13 @@ import type {
     ApplicationRenderContext,
     ApplicationRenderOptions,
 } from "../_types.d.ts";
+import EventEmitterMixin from "../../../common/utils/event-emiiter.ts";
 
 /** The Application class is responsible for rendering an HTMLElement into the Foundry Virtual Tabletop user interface. */
 export default abstract class ApplicationV2<
     TConfig extends ApplicationConfiguration = ApplicationConfiguration,
     TRenderOptions extends ApplicationRenderOptions = ApplicationRenderOptions
-> {
+> extends EventEmitterMixin(Object) {
     constructor(options?: DeepPartial<TConfig>);
 
     /**
