7281644f46bfe399779a600fcf34f02fc4f75577
Idle <651462+reonZ@users.noreply.github.com>
2024-11-24 19:26:45 +0100

    remove jsdoc and add override

--- types/foundry/client/pixi/core/interaction/targets.d.ts
+++ types/foundry/client/pixi/core/interaction/targets.d.ts
@@ -3,7 +3,6 @@ export {};
 declare global {
     /**
      * A subclass of Set which manages the Token ids which the User has targeted.
-     * @extends {Set}
      * @see User#targets
      */
     class UserTargets<TToken extends Token> extends Set<TToken> {
@@ -11,17 +10,13 @@ declare global {
 
         /**
          * Return the Token IDs which are user targets
-         * @type {string[]}
          */
         get ids(): string[];
 
-        /** @override */
-        add(token: TToken): this;
+        override add(token: TToken): this;
 
-        /** @override */
-        clear(): void;
+        override clear(): void;
 
-        /** @override */
-        delete(token: TToken): boolean;
+        override delete(token: TToken): boolean;
     }
 }
