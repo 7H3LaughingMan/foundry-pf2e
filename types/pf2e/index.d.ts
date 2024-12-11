9549ee2427393fbe95b006a94ce82232e3c9f778
Idle <651462+reonZ@users.noreply.github.com>
2024-12-11 22:23:30 +0100

    add User & JournalEntry to getDocumentClass

--- types/pf2e/index.d.ts
+++ types/pf2e/index.d.ts
@@ -4,13 +4,16 @@ import { ChatMessagePF2e } from "./module/chat-message/document.js";
 import { CombatantPF2e } from "./module/encounter/combatant.js";
 import { ItemPF2e } from "./module/item/index.js";
 import { MacroPF2e } from "./module/macro.js";
+import { UserPF2e } from "./module/user/document";
 
 declare global {
     function getDocumentClass(name: "ChatMessage"): typeof ChatMessagePF2e;
+    function getDocumentClass(name: "JournalEntry"): typeof JournalEntry;
     function getDocumentClass(name: "Combatant"): typeof CombatantPF2e;
     function getDocumentClass(name: "Macro"): typeof MacroPF2e;
     function getDocumentClass(name: "Actor"): typeof ActorPF2e;
     function getDocumentClass(name: "Item"): typeof ItemPF2e;
+    function getDocumentClass(name: "User"): typeof UserPF2e;
 }
 
 export {};
