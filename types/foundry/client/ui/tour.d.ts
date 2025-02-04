42355a356aef0db5f24c101231580557ce1f6381
Spappz <34202141+Spappz@users.noreply.github.com>
2025-02-04 15:10:42 +0000

    Two more `TourStep` properties! (#603)
    * Update tour.d.ts
    
    * Update tour.d.ts

--- types/foundry/client/ui/tour.d.ts
+++ types/foundry/client/ui/tour.d.ts
@@ -189,8 +189,12 @@ declare global {
         /** A DOM selector which denotes an element to highlight during this step.
          *  If omitted, the step is displayed in the center of the screen. */
         selector?: string;
-        /** Activates a particular sidebar tab. Only works in `SidebarTour` instances. */
+        /** Activates a particular sidebar tab. Only usable in `SidebarTour` instances. */
         sidebarTab?: string;
+        /** Activates a particular canvas layer and its respective control group. Only usable in `CanvasTour` instances. */
+        layer?: string;
+        /** Activates a particular tool. Requires `layer` to be defined to have any effect. Only usable in `CanvasTour` instances. */
+        tool?: string;
         /** How the tooltip for the step should be displayed relative to the target element.
          *  If omitted, the best direction will be attempted to be auto-selected.
          */
